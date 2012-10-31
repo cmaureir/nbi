99e828ffc3cbc2f8409b1aaec40b97e831503765
Cristian Maureira Fredes <cmaureir@csrg.inf.utfsm.cl>
2012-10-31 17:20:10 +0100

    Adding empty files

---
+++ lib/plummer_model.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+class PlummerSphere:
+    def __init__(self):
+        pass
+    def get_density(self):
+        pass
+    def get_potential(self):
+        pass
+
+
+def get_plummer_sphere():
+    ps = PlummerSphere()
---
+++ plummer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# Plummer Model Generator
+# Author: Cristian Maureira
+# Date: 31/10/2012
+
+import argparse
+from lib.options_parser import *
+from lib.plummer_model  import *
+
+def main():
+    # Parse options
+    get_plummer_sphere()
+
+if __name__ == "__main__":
+    main()
