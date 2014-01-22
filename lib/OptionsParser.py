9ea2b9b8b743b81918ea3c55c316e7296fa0c0ab
Cristian Maureira-Fredes <cmaurei@aei.mpg.de>
2014-01-22 13:15:19 +0100

    Adding some order into the code, still unusable
    Signed-off-by: Cristian Maureira-Fredes <cmaurei@aei.mpg.de>

--- lib/OptionsParser.py
+++ lib/OptionsParser.py
@@ -2,18 +2,99 @@ import argparse
 
 class OptionsParser:
     def __init__(self):
-        parser = argparse.ArgumentParser(description='Create a circular, plane, stellar disc')
-#parser.add_argument('-i',metavar='infile',dest='infile',help='Input file', required=True)
-parser.add_argument('-o',metavar='outfile',dest='outfile',help='Name of output ascii file',default="disc.dat")
-#parser.add_argument('--format',dest='format',type=int,help='Input format. 0 = Shuo, 1 = gsplash ascii. Default = 0', default=0)
-parser.add_argument('-u',dest='ulim',type=float,help='Outer radius for disc distribution [pc]',required=True)
-parser.add_argument('-l',dest='llim',type=float,help='Inner radius for disc distribution [pc]',required=True)
-parser.add_argument('-n',dest='nmp',type=int,help='Number of stars',required=True)
-parser.add_argument('-mbh',dest='mbh',type=float,help='SMBH mass [Msun]',required=True)
-parser.add_argument('-b',dest='beta',type=float,help='Surface density exponent, Sigma propto r^(-beta). If we are creating not a disk but a sphere, this should be 3D exponent - 1.',required=True)
-parser.add_argument('-d',dest='disc',type=int,help='0: disc,  1: spherical isotropic, requires also argument for -e distribution',default=0)
-parser.add_argument('-m',dest='mmp',type=float,help='Mass of stars [Msun]',required=True)
-parser.add_argument('-e',dest='gamma',type=float,help='Power law index gamma of eccentricity distribution, default = 1.0 (thermalized)',default=1.0,required=False)
-parser.add_argument('--profile',dest='profile',help='Print the density profile to this file',default=None)
-parser.add_argument('--plot',dest='plot',help='plot histogram of density',action='store_true')
-args = parser.parse_args()
+
+        self.parser  = None
+        desc = "Create a circular, plane, stellar disc"
+        self.parser = argparse.ArgumentParser(description=desc)
+        #self.parser = argparse.ArgumentParser(description=desc,
+        #                                      formatter_class=argparse.RawTextHelpFormatter)
+        self.add_arguments()
+
+    def add_arguments(self):
+        out_help = "Name of output ascii file"
+        self.parser.add_argument("-o",
+                                 metavar = "outfile",
+                                 dest    = "outfile",
+                                 help    =  out_help,
+                                 default = "disc.dat")
+
+
+        ulim_help = "Outer radius for disc distribution [pc]"
+        self.parser.add_argument("-u",
+                                 dest     = "ulim",
+                                 type     = float,
+                                 help     = ulim_help,
+                                 required = True)
+
+        llim_help = "Inner radius for disc distribution [pc]"
+        self.parser.add_argument("-l",
+                                 dest     = "llim",
+                                 type     = float,
+                                 help     = llim_help,
+                                 required = True)
+
+        nmp_help = "Number of stars"
+        self.parser.add_argument("-n",
+                                 dest     = "nmp",
+                                 type     = int,
+                                 help     = nmp_help,
+                                 required = True)
+
+        mbh_help = "SMBH mass [Msun]"
+        self.parser.add_argument("-mbh",
+                                 dest     = "mbh",
+                                 type     = float,
+                                 help     = mbh_help,
+                                 required = True)
+
+        beta_help = "Surface density exponent, Sigma propto r^(-beta).\
+                     If we are creating not a disk but a sphere, this should\
+                     be 3D exponent - 1."
+        self.parser.add_argument("-b",
+                                 dest     = "beta",
+                                 type     = float,
+                                 help     = beta_help,
+                                 required = True)
+
+        disc_help = "0: disc,\
+                     1: spherical isotropic,\
+                     requires also argument for -e distribution"
+        self.parser.add_argument("-d",
+                                 dest    = "disc",
+                                 type    = int,
+                                 help    = disc_help,
+                                 default = 0)
+
+        mmp_help = "Mass of stars [Msun]"
+        self.parser.add_argument("-m",
+                                 dest     = "mmp",
+                                 type     = float,
+                                 help     = mmp_help,
+                                 required = True)
+
+        gamma_help = "Power law index gamma of eccentricity distribution,\
+                      default = 1.0 (thermalized)"
+        self.parser.add_argument("-e",
+                                 dest     = "gamma",
+                                 type     = float,
+                                 help     = gamma_help,
+                                 default  = 1.0,
+                                 required = False)
+
+        profile_help = "Print the density profile to this file"
+        self.parser.add_argument("--profile",
+                                 dest    = "profile",
+                                 help    = profile_help,
+                                 default = None)
+
+        plot_help = "plot histogram of density"
+        self.parser.add_argument("--plot",
+                                 dest   = "plot",
+                                 help   = plot_help,
+                                 action = "store_true")
+    def process_args(self):
+        # Do something with self.parser
+        pass
+
+    def get_args(self):
+        return self.parser.parse_args()
--- nbi.py
+++ nbi.py
@@ -1,11 +1,79 @@
 #!/usr/bin/env python
 
-from lib.options_parser import *
-from lib.plummer_model  import *
-from lib.disc  import *
+from lib.OptionsParser import *
+from lib.PlummerModel  import *
+from lib.DiscModel  import *
+
+def plot(plot):
+    if plot:
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.hist(eccs,bins=40,cumulative=True,normed=True,alpha=0.5)
+    #    ax.hist(eccs_small_a,bins=40,cumulative=True,normed=True,alpha=0.5)
+    #    ax.hist(random_eccs,bins=10,cumulative=True,normed=True,alpha=0.5)
+        ax.plot(np.arange(0.0,1.0,0.01),np.power(np.arange(0.0,1.0,0.01),2.0))
+        ax.plot(np.arange(0.0,1.0,0.01),np.power(np.arange(0.0,1.0,0.01),3.6))
+        print np.arange(0.0,1.0,0.01)
+    #    ax.hist(messured_a,bins=40,alpha=0.5)
+        plt.show()
+
 
 def main():
     # Parse options
+    op = OptionsParser()
+    op = op.get_args()
+
+    disc  = op.disc
+    gamma = op.gamma
+    outfile = op.outfile
+
+    #create massive perturbers
+    nmp  = op.nmp
+    mmp  = op.mmp
+    mbh  = op.mbh
+    rmax = op.ulim
+    rmin = op.llim
+    beta = op.beta
+
+    #draw semi-major axes from a power law distribution
+    #the numpy.random.power uses \propto a*x^(a-1)
+    radii_temp = np.random.power(2.0-beta,nmp)
+    radii      = np.sort(radii_temp)
+
+    if disc == 0:
+        mps = create_star_orbits(radii,rmin,rmax,beta,nmp,mmp,mbh)
+
+    elif disc == 1:
+        eccs = np.random.power(gamma+1.0,nmp)
+        mps = create_isotropic_orbits(radii,eccs,rmin,rmax,beta,nmp,mmp,mbh)
+
+    #    messured_eccs, messured_a = confirm_orbits(nmp,mps,mbh)
+    #open file and write output
+
+    ofile = open(outfile,'w')
+    outstring = '{0:10e} {1:10e} {2:10e} {3:10e} \
+                 {4:10e} {5:10e} {6:10e}\n'.format(mbh,0.0,0.0,0.0,0.0,0.0,0.0)
+    ofile.write(outstring)
+
+    for mp in mps:
+        outstring = '{0:10e} {1:10e} {2:10e} {3:10e} \
+                     {4:10e} {5:10e} {6:10e}\n'.format(float(mp[0]),\
+                                                       float(mp[1]),\
+                                                       float(mp[2]),\
+                                                       float(mp[3]),\
+                                                       float(mp[4]),\
+                                                       float(mp[5]),\
+                                                       float(mp[6]))
+        ofile.write(outstring)
+
+    ofile.close()
+    random_eccs_ids = random.sample(range(args.nmp),nmp)
+    random_eccs = []
+    for i in range(nmp):
+        random_eccs.append(eccs[random_eccs_ids[i]])
+    plot(op.plot, eccs)
 
 if __name__ == "__main__":
     main()
+
+
