a87a57b5d9d43cf4bc1fe378985d0ed2844f7d40
Cristian Maureira-Fredes <cmaurei@aei.mpg.de>
2014-01-28 16:18:41 +0100

    Adding spherical/adjust functions
    Signed-off-by: Cristian Maureira-Fredes <cmaurei@aei.mpg.de>

--- lib/PlummerModel.py
+++ lib/PlummerModel.py
@@ -1,13 +1,81 @@
 #!/usr/bin/env python
 
+import random
+import numpy as np
+
+
 class PlummerSphere:
     def __init__(self):
-        pass
-    def get_density(self):
-        pass
-    def get_potential(self):
-        pass
+
+        self.mass = []
+        self.pos = []
+        self.vel = []
+
+        self.epot = 0.0
+        self.ekin = 0.0
+
+        self.N = 1024
+        self.M = 1.0
+        self.E = -0.25
+        self.r = np.array([0,0,0])
+        self.v = np.array([0,0,0])
+        self.a = 1
+
+        self.scale_factor = 16.0/(3.0 * np.pi)
+
+        self.cummulative_mass_min = 0
+        self.cummulative_mass_max = 1.0/self.N
+
+    def spherical(self, r):
+
+        vector = np.array([0.0, 0.0, 0.0])
+        theta = np.arccos(random.uniform(-1, 1))
+        phi = random.uniform(0, 2*np.pi)
+
+        vector[0] = r * np.sin( theta ) * np.cos( phi )
+        vector[1] = r * np.sin( theta ) * np.sin( phi )
+        vector[2] = r * np.cos( theta )
+
+        return vector
 
 
-def get_plummer_sphere():
-    ps = PlummerSphere()
+    def create_model(self):
+        for i in range(self.N):
+            m = self.M/self.N
+            self.cummulative_mass = random.uniform(self.cummulative_mass_min, self.cummulative_mass_max)
+            self.cummulative_mass_min = self.cummulative_mass_max
+            self.cummulative_mass_max += m
+
+            radius = 1 / np.sqrt(self.cummulative_mass ** (-2.0/3.0) - 1.0)
+
+            r = self.spherical(radius)/self.scale_factor
+
+            x = 0.0
+            y = 0.1
+            while y > (x*x) * (1.0- x*x)**3.5:
+                x = random.uniform(0,1)
+                y = random.uniform(0,0.1)
+
+            velocity = x * np.sqrt(2.0) * ( 1.0 + radius * radius)**(-0.25)
+
+            v = self.spherical(velocity) * np.sqrt(self.scale_factor)
+
+            #print m, r[0], r[1], r[2], v[0], v[1], v[2]
+            self.mass.append(m)
+            self.pos.append(r)
+            self.vel.append(v)
+
+
+
+
+
+if __name__ == "__main__":
+    p = PlummerSphere()
+    p.create_model()
+    p.get_energy(p.pos, p.vel,p.mass)
+    p.adjust_center_of_mass()
+    print "--------------"
+    p.get_energy(p.pos, p.vel,p.mass)
+    p.adjust_units()
+    print "--------------"
+    p.get_energy(p.pos, p.vel,p.mass)
--- lib/Utils.py
+++ lib/Utils.py
@@ -1,51 +1,109 @@
+def get_energy(pos, vel, mass, N):
+
+    # Total Potential and Kinetic Energy
+    epot = 0.0
+    ekin = 0.0
+
+    # Temporal values
+    t_epot = 0.0
+    t_ekin = 0.0
+
+    for i in range(N):
+        t_epot = 0
+        for j in range(N):
+            if i != j:
+                rx = pos[j][0] - pos[i][0]
+                ry = pos[j][1] - pos[i][1]
+                rz = pos[j][2] - pos[i][2]
+                rinv = 1 / np.sqrt(rx*rx + ry*ry + rz*rz)
+
+                t_epot -= (mass[i] * mass[j]) * rinv
+        t_ekin = 0.5 * mass[i] * (vel[i][0]**2 + vel[i][1]**2 + vel[i][2]**2)
+
+        epot += 0.5 * t_epot
+        ekin += t_ekin
+
+    print("Epot:", self.epot)
+    print("Ekin:", self.ekin)
+    print("Etotal:", self.epot + self.ekin)
+
+def adjust_center_of_mass(pos, vel, mass, N):
+    vel_com = np.zeros(3)
+    pos_com = np.zeros(3)
+
+    for i in range(self.N):
+        pos_com[0] += pos[i][0] * mass[i]
+        pos_com[1] += pos[i][1] * mass[i]
+        pos_com[2] += pos[i][2] * mass[i]
+
+        vel_com[0] += vel[i][0] * mass[i]
+        vel_com[1] += vel[i][1] * mass[i]
+        vel_com[2] += vel[i][2] * mass[i]
+
+    for i in range(self.N):
+        pos[i][0] -= pos_com[0]
+        pos[i][1] -= pos_com[1]
+        pos[i][2] -= pos_com[2]
+
+        vel[i][0] -= vel_com[0]
+        vel[i][1] -= vel_com[1]
+        vel[i][2] -= vel_com[2]
 
 
-    def confirm_orbits(nmp,mps,mu):
-        eccs = np.zeros(nmp)
-        semis = np.zeros(nmp)
-        for i in range(nmp):
-            mp = mps[i]
-            x = mp[1:4]
-            v = mp[4:7]
+def adjust_units(pos, vel, N, ekin, epot):
+    alpha = -epot / 0.5
+    beta  =  ekin / 0.25
 
-            # r magnitude
-            x_mag = np.linalg.norm(x)
+    for i in range(self.N):
+        pos[i] *= alpha
+        vel[i] /= np.sqrt(beta)
 
-            # Angular momentum
-            # j = r x v
-            j = np.cross(x, v)
+def confirm_orbits(nmp,mps,mu):
+    eccs = np.zeros(nmp)
+    semis = np.zeros(nmp)
+    for i in range(nmp):
+        mp = mps[i]
+        x = mp[1:4]
+        v = mp[4:7]
 
-            # Runge-Lenz-vector
-            # e = { (v x j) / (G * m) }  - { r / |r| }
-            e = np.cross(v, j)/(G * mu) - x/x_mag
+        # r magnitude
+        x_mag = np.linalg.norm(x)
 
-            # Eccentricity
-            ecc = np.linalg.norm(e)
+        # Angular momentum
+        # j = r x v
+        j = np.cross(x, v)
 
-            # Semi-major axis
-            # a = ( j * j ) / (G * m * | 1 - ecc^2 | )
-            a = j.dot(j)/(G* mu * np.fabs(1-ecc**2))
+        # Runge-Lenz-vector
+        # e = { (v x j) / (G * m) }  - { r / |r| }
+        e = np.cross(v, j)/(G * mu) - x/x_mag
 
-            semis[i] = a
-            eccs[i] = ecc
-        return eccs, semis
+        # Eccentricity
+        ecc = np.linalg.norm(e)
 
-    def print_profile(m,r,fname):
-        """Print the density profile
+        # Semi-major axis
+        # a = ( j * j ) / (G * m * | 1 - ecc^2 | )
+        a = j.dot(j)/(G* mu * np.fabs(1-ecc**2))
 
-        Arguments:
-        - `m`:
-        - `r`:
-        - `fname`:
-        """
-        #find out what dr is
-        ofile = open(fname,'w')
-        dr = r[1] - r[0]
-        volfac = dr*4.0*np.pi
-        for i in range(1,len(r)):
-            radius = r[i]
-            mass = m[i]
-            dens = mass /( radius*radius*volfac)
-            outline = "{0:.6e} {1:.6e}\n".format(radius,dens)
-            ofile.write(outline)
-        ofile.close()
+        semis[i] = a
+        eccs[i] = ecc
+    return eccs, semis
+
+def print_profile(m,r,fname):
+    """Print the density profile
+
+    Arguments:
+    - `m`:
+    - `r`:
+    - `fname`:
+    """
+    #find out what dr is
+    ofile = open(fname,'w')
+    dr = r[1] - r[0]
+    volfac = dr*4.0*np.pi
+    for i in range(1,len(r)):
+        radius = r[i]
+        mass = m[i]
+        dens = mass /( radius*radius*volfac)
+        outline = "{0:.6e} {1:.6e}\n".format(radius,dens)
+        ofile.write(outline)
+    ofile.close()
